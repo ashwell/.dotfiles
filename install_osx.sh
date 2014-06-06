c9560f1186bb61174dad9d58927aad322b7ac3bd
Ryan <Ashwell@users.noreply.github.com>
2014-06-05 19:48:03 -0700

    Update install_osx.sh

--- install_osx.sh
+++ install_osx.sh
@@ -1,8 +1,33 @@
 #!bin/sh
 
 # install xcode
+xcode-select --install
+
 # install homebrew
-# brew install [bash, bash-completion, git, node]
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+# brew install stuffs: [bash, bash-completion, git, node]
+
+# install bash
+brew install bash
 # setup homebrew bash
+# Add the new shell to the list of legit shells
+sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
+# Change the shell for the user
+chsh -s /usr/local/bin/bash
+# Check for Bash 4 and /usr/local/bin/bash...
+echo $BASH && echo $BASH_VERSION
+
+
+#brew install bash-completion
+
+#brew install git
+
+#brew install node
+
+
 # pull down .dotfiles
+#cd ~
+#git clone git@github.com:Ashwell/.dotfiles.git
+
 # install .dotfiles (link step)
