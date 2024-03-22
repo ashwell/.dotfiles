09e2e0fc518887bf71762e2d9f5d3dbfa0f2c342
Ryan Bogle <ryan.bogle@unitedtalent.com>
2024-03-21 17:02:04 -0700

    More MacOS settings

--- install_osx.sh
+++ install_osx.sh
@@ -37,3 +37,13 @@ sudo cp ~/.dotfiles/mac/DefaultKeyBinding.dict ~/Library/KeyBindings/
 
 # Enable Key Repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# show Library folder
+chflags nohidden ~/Library
+# show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+# show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+# show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+# do not open previous previewed files (e.g. PDFs) when opening a new one
+defaults write com.apple.Preview ApplePersistenceIgnoreState YES
